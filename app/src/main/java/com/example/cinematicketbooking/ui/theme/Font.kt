54782f10e78984cdb6b12a68b4aafba7d5e4cdc7
Eslam <eslam.mohamed.551999@gmail.com>
2025-06-12 17:00:19 +0300

    update font

--- app/src/main/java/com/example/cinematicketbooking/ui/theme/Font.kt
+++ app/src/main/java/com/example/cinematicketbooking/ui/theme/Font.kt
@@ -5,8 +5,10 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import com.example.cinematicketbooking.R
 
-val SFPro = FontFamily(
-    Font(R.font.sf_pro_regular, weight = FontWeight.Normal),
-    Font(R.font.sf_pro_medium, weight = FontWeight.Medium),
-    Font(R.font.sf_pro_bold, weight = FontWeight.Bold),
+val Avenir = FontFamily(
+    Font(R.font.avenir_light, weight = FontWeight.Light),
+    Font(R.font.avenir_book, weight = FontWeight.Normal),
+    Font(R.font.avenir_medium, weight = FontWeight.Medium),
+    Font(R.font.avenir_heavy, weight = FontWeight.Bold),
+    Font(R.font.avenir_black, weight = FontWeight.Black),
 )
